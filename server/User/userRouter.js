b23b34a5c3885b9e1d61888415541764c0510c8b
Katrina Uychaco <kuychaco@gmail.com>
2015-08-07 19:51:05 -0700

    Refactor to use app.route rather than app.get

--- server/User/userRouter.js
+++ server/User/userRouter.js
@@ -13,37 +13,41 @@ module.exports = function(app, passport) {
 
   // Google Routes
   // Send to google to do authentication
-  app.get('/auth/google', 
-    passport.authenticate('google', { scope: ['profile', 'email'] }));
+  app.route('/auth/google')
+    .get(passport.authenticate('google', { scope: ['profile', 'email'] }));
 
   // The callback after google has authenticated the user
-  app.get('/auth/google/callback',
-    passport.authenticate('google', {
-      successRedirect: '/api/user/profile',
-      failureRedirect: '/api/user'
-    })); 
+  app.route('/auth/google/callback')
+    .get(passport.authenticate('google', {
+        successRedirect: '/api/user/profile',
+        failureRedirect: '/api/user'
+      }));
 
   // Home Page
-  app.get('/', isLoggedIn, function(req, res) {
-    res.render('profile.ejs',  { user: req.user });
-  });
+  app.route('/')
+    .get(isLoggedIn, function(req, res) {
+      res.render('profile.ejs',  { user: req.user });
+    });
 
   // Profile section 
   // Use route middleware to verify user is logged in
-  app.get('/profile', isLoggedIn, function(req, res) {
-    // Get the user out of the session and pass to template
-    res.render('profile.ejs', { user: req.user });
-  });
+  app.route('/profile')
+    .get(isLoggedIn, function(req, res) {
+      // Get the user out of the session and pass to template
+      res.render('profile.ejs', { user: req.user });
+    });
 
   // Login
-  app.get('/login', function(req, res) {
-    res.render('login.ejs');
-  });
+  app.route('/login')
+    .get(function(req, res) {
+      res.render('login.ejs');
+    });
 
   // Logout
-  app.get('/logout', function(req, res) {
-    req.logout();
-    res.redirect('/');
-  });
+  app.route('/logout')
+    .get(function(req, res) {
+      req.logout();
+      res.redirect('/');
+    });
 
 };
