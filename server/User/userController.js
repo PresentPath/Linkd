c1ec30cfcbae24bf7eaa8fefcbcedaa928cee385
Katrina Uychaco <kuychaco@gmail.com>
2015-08-07 19:59:55 -0700

    Add comments

--- server/User/userController.js
+++ server/User/userController.js
@@ -7,25 +7,31 @@
 
 var User = require('../config/db_models.js').User;
 
-
+// Check if user is in database
 module.exports.checkUserAuth = function(profile, token, callback) {
+
   User.find({ where: {user_id_google: profile.id} })
-  .then(function(user) {
-    if (user) {
-      console.log('User', user.get({plain:true}).name_google, 'already exists');
-      callback(null, user);
-    }
-    if (user === null) {
-      module.exports.createUserAuth(profile, token, callback);
-    }
-  })
-  .error(function(err) {
-    console.error('Error finding user:', err);
-    callback(err);
-  });
+    .then(function(user) {
+      if (user) {
+        console.log('User', user.get({plain:true}).name_google, 'already exists');
+        callback(null, user);
+      }
+
+      // If user not found, create user
+      if (user === null) {
+        module.exports.createUserAuth(profile, token, callback);
+      }
+    })
+    .error(function(err) {
+      console.error('Error finding user:', err);
+      callback(err);
+    });
 };
 
+
+// Create new user in database
 module.exports.createUserAuth = function(profile, token, callback) {
+
   User.create({
     user_id_google: profile.id,
     token_google: token,
