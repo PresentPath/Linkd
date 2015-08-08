b36114d0a1f08c465ae6562ba45dc9394953e6c9
Katrina Uychaco <kuychaco@gmail.com>
2015-08-07 20:30:24 -0700

    Add skeleton code for controller methods

--- server/Group/groupController.js
+++ server/Group/groupController.js
@@ -3,7 +3,34 @@
 //
 // Handle interacting with the Group data in the database.
 
-module.exports = {
+var Group = require('../config/db_models.js').Group;
 
+// Retrive list of all Group instances
+module.exports.getGroupsList = function(req, res, next) {
+
+};
+
+// Create a new Group instance in the database based on data sent in the request
+module.exports.createGroup = function(req, res, next) {
+
+};
+
+// Retrieve memebers in a specific Group instance
+module.exports.getGroupMembers = function(req, res, next) {
+
+};
+
+// Add a user to a Group
+module.exports.addUserToGroup = function(req, res, next) {
+
+};
+
+// Delete a Group Instance from the database
+module.exports.deleteGroup = function(req, res, next) {
+
+};
+
+// Rename Group
+module.exports.renameGroup = function(req, res, next) {
 
 };
