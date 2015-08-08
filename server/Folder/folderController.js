3e3e253d4fce7c595f1e5bad8e8aa73fde14bb4a
Katrina Uychaco <kuychaco@gmail.com>
2015-08-07 20:38:33 -0700

    Add skeleton code for controller methods

--- server/Folder/folderController.js
+++ server/Folder/folderController.js
@@ -3,7 +3,24 @@
 //
 // Handle interacting with the Folder data in the database.
 
-module.exports = {
+var Folder = require('../config/db_models.js').Folder;
 
+// Retrieve list of subFolders for a given parent folder
+module.exports.getSubFolders = function(req, res, next) {
+
+};
+
+// Create a new folder instance in the database
+module.exports.createFolder = function(req, res, next) {
+
+};
+
+// Delete folder instance from database
+module.exports.deleteFolder = function(req, res, next) {
+
+};
+
+// Rename folder in database
+module.exports.renameFolder = function(req, res, next) {
 
 };
