f00ad93ef0a860c3dd0bec6b770c6984f2f52d93
Katrina Uychaco <kuychaco@gmail.com>
2015-08-08 01:43:13 -0700

    Refactor - separate find and create functionality

--- server/User/userController.js
+++ server/User/userController.js
@@ -10,24 +10,41 @@ var User = require('../config/db_models.js').User;
 // Check if user is in database
 module.exports.checkUserAuth = function(profile, token, callback) {
 
-  User.findOrCreate({ where: {
+  User.find({ where: {user_id_google: profile.id} })
+    .then(function(user) {
+      if (user) {
+        console.log('User', user.get({plain:true}).name_google, 'already exists');
+        callback(null, user);
+      }
+
+      // If user not found, create user
+      if (user === null) {
+        module.exports.createUserAuth(profile, token, callback);
+      }
+    })
+    .error(function(err) {
+      console.error('Error finding user:', err);
+      callback(err);
+    });
+};
+
+// Create new user in database
+module.exports.createUserAuth = function(profile, token, callback) {
+
+  User.create({
     user_id_google: profile.id,
     token_google: token,
     name_google: profile.displayName,
     email_google: profile.emails[0].value
-  } })
-  .spread(function(user, created) {
-    // console.log('New user:', user.get({
-    //   plain: true
-    // }));
-    console.log('New user created:', created);
+  })
+  .then(function(user) {
+    console.log('User', user.get({plain:true}).name_google, 'created');
     callback(null, user);
   })
   .error(function(err) {
     console.error('Error creating user:', err);
     callback(err);
   });
-
 };
 
 // Retrieve list of all users
