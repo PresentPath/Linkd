5df1ecb8a442112c6f090a1f3701ac1013267a96
Katrina Uychaco <kuychaco@gmail.com>
2015-08-07 22:46:39 -0700

    Add getGroupList and createGroup methods

--- server/Group/groupController.js
+++ server/Group/groupController.js
@@ -7,21 +7,48 @@ var Group = require('../config/db_models.js').Group;
 
 // Retrive list of all Group instances
 module.exports.getGroupsList = function(req, res, next) {
+  console.log('SessionInfo', req.session.passport.user);
+  console.log('UserInfo', req.user);
+
+  Group.findAll({ where: { OwnerUserIdGoogle: req.session.passport.user } })
+  .then(function(groups) {
+    console.log('Successfully retrieved groups list for user');
+    res.json(groups);
+  })
+  .error(function(err) {
+    console.error('Error in retrieving groups list from database', err);
+    res.status(500).send(err);
+  });
 
 };
 
 // Create a new Group instance in the database based on data sent in the request
 module.exports.createGroup = function(req, res, next) {
+  console.log('New Group', req.body);
 
-};
-
-// Retrieve memebers in a specific Group instance
-module.exports.getGroupMembers = function(req, res, next) {
+  Group.create({
+    name: req.body.name,
+    OwnerUserIdGoogle: req.session.passport.user
+  })
+  .then(function(group) {
+    console.log('Successfully created group in database');
+    // Send group object back to client as JSON
+    res.json(group);
+  })
+  .error(function(err) {
+    console.error('Error in creating new group in database:', err);
+    res.status(500).send(err);
+  });
 
 };
 
 // Add a user to a Group
 module.exports.addUserToGroup = function(req, res, next) {
+
+};
+
+// Retrieve memebers in a specific Group instance
+module.exports.getGroupMembers = function(req, res, next) {
 
 };
 
