266f5ef3b7545d0774bacb4affd0f2e0978f3ecc
Katrina Uychaco <kuychaco@gmail.com>
2015-08-07 20:29:20 -0700

    Add group routes

--- server/Group/groupRouter.js
+++ server/Group/groupRouter.js
@@ -8,4 +8,29 @@ var groupController = require('./groupController.js');
 
 module.exports = function(app) {
 
+  // Send back groups
+  app.route('/list')
+    .get(groupController.getGroupsList);
+
+  // Create group
+  app.route('/create')
+    .post(groupController.createGroup);
+
+  // Send back users in a specific group
+  app.route('/:groupId')
+    .get(groupController.getGroupMembers);
+
+  // Add user to group
+  app.route('/addUser/:groupId/:userId')
+    .post(groupController.addUserToGroup);
+
+  // Delete group
+  // TODO: Research DELETE HTTP method
+  app.route('/delete/:groupId')
+    .get(groupController.deleteGroup);
+
+  // Rename group
+  app.route('/rename/:groupId')
+    .post(groupController.renameGroup);
+
 };
