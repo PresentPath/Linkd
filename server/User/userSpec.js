8d6485f6df7fe6756c592e2ad3da46ce295aaa0c
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-08-10 23:11:43 -0700

    Refactor User spec file to do test data setup through Sequelize.
    - Test data setup is separate from actual testing of server.

--- server/User/userSpec.js
+++ server/User/userSpec.js
@@ -7,75 +7,85 @@ var request = require('supertest');
 var expect = require('chai').expect;
 var app = require('../serverSetup.js');
 var Promise = require('bluebird');
+var User = require('../config/db_models.js').User;
 
-var userController = Promise.promisifyAll(require('./userController.js'));
+// User Controller
+describe('----- User Router/Controller tests -----', function() {
 
-module.exports.addThenRetrieveUsers = function(callback) {
+  before(function(done) {
+    // Fake user data
+    var users = [
+      {
+        user_id_google: '102064592924036401703',
+        name_google: 'Katrina Uychaco',
+        email_google: 'kuychaco@gmail.com',
+        token_google: 'ya29.yQH7RHzsmcgSg0kI_eSLniVA14lXL6kaK8fQwuWiUOk-IyeosYDs3tBagxJYrJeS95_GJg'
+      },
+      {
+        user_id_google: '103256925994420229365',
+        name_google: 'Katrina Uychaco',
+        email_google: 'uychacok@gmail.com',
+        token_google: 'ya29.yQF874WkXayqM7cLgNjbum_P4k2NojVzTQ6OInoQEorrnItI2NGM5_6ul4OaulXpSqKK'
+      },
+      {
+        user_id_google: '108778785915485593543',
+        name_google: 'Katrina Uychaco',
+        email_google: 'katrina.uychaco@telegraphacademy.com',
+        token_google: 'ya29.yQHaRmj6uzDFAllN-p0gzOLSEW9y0z8xmXT3WtNgKizpF_QtHjd5WoU7ksTVGtXPh_Qg'
+      }
+    ];
 
-  // User Controller
-  describe('----- User Router/Controller tests -----', function() {
-    // Add 3 users to database
-    it('should add and retrieve users from the database', function(done) {
 
-      // Fake user data
-      var users = [
-        {
-          profile: {
-            id: '102064592924036401703',
-            displayName: 'Katrina Uychaco',
-            emails: [{ value: 'kuychaco@gmail.com' }]
-          },
-          token: 'ya29.yQH7RHzsmcgSg0kI_eSLniVA14lXL6kaK8fQwuWiUOk-IyeosYDs3tBagxJYrJeS95_GJg'
-        },
-        {
-          profile: {
-            id: '103256925994420229365',
-            displayName: 'Katrina Uychaco',
-            emails: [{ value: 'uychacok@gmail.com' }]
-          },
-          token: 'ya29.yQF874WkXayqM7cLgNjbum_P4k2NojVzTQ6OInoQEorrnItI2NGM5_6ul4OaulXpSqKK'
-        },
-        {
-          profile: {
-            id: '108778785915485593543',
-            displayName: 'Katrina Uychaco',
-            emails: [{ value: 'katrina.uychaco@telegraphacademy.com' }]
-          },
-          token: 'ya29.yQHaRmj6uzDFAllN-p0gzOLSEW9y0z8xmXT3WtNgKizpF_QtHjd5WoU7ksTVGtXPh_Qg'
-        },
-      ];
-
-      // Add users to database
-      Promise.map(users, function(user) {
-        return userController.checkUserAuthAsync(user.profile, user.token);
+    User.findAll()
+      .then(function(users){
+        return Promise.map(users, function(user){
+          return user.destroy();
+        });
+      })
+      .then(function(affectedRows) {
+        return User.bulkCreate(users);
       })
       .then(function(users) {
-        // console.log(users);
-
-        // Get user list and verify users added
-        request(app)
-          .get('/api/user/list')
-          .end(function(err, res) {
-            if (err) {
-              done(err);
-              callback(err);
-            }
-
-            expect(res.body.length).to.equal(3);
-
-            expect(res.body.filter(function(user) {
-              return user.name_google === 'Katrina Uychaco';
-            }).length).to.be.above(0);
-            done();
-            callback(null, 'User Controller tests complete');
-          });
+        console.log('Created test users successfully');
+        done();
       })
-      .catch(function(err) {
+      .error(function(err) {
+        console.error(err.message);
         done(err);
-        callback(err);
       });
 
-    });
   });
 
-};
+  // Add 3 users to database
+  it('should add and retrieve users from the database', function(done) {
+
+
+    // Add users to database
+    // Promise.map(users, function(user) {
+    //   return userController.checkUserAuthAsync(user.profile, user.token);
+    // })
+    // .then(function(users) {
+    //   // console.log(users);
+
+      // Get user list and verify users added
+      request(app)
+        .get('/api/user/list')
+        .end(function(err, res) {
+          if (err) {
+            done(err);
+          }
+
+          expect(res.body.length).to.equal(3);
+
+          expect(res.body.filter(function(user) {
+            return user.name_google === 'Katrina Uychaco';
+          }).length).to.be.above(0);
+          done();
+        });
+    // })
+    // .catch(function(err) {
+    //   done(err);
+    // });
+
+  });
+});
