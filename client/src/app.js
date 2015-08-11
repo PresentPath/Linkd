c1be47cc60d17ca62580bffda0b7c1793bad03f5
Katrina Uychaco <kuychaco@gmail.com>
2015-08-10 21:32:35 -0700

    Remove header comments

--- client/src/app.js
+++ client/src/app.js
@@ -1,10 +1,3 @@
-/*
-* @Author: kuychaco
-* @Date:   2015-08-07 20:59:08
-* @Last Modified by:   kuychaco
-* @Last Modified time: 2015-08-08 12:29:24
-*/
-
 'use strict';
 
 $('document').ready(function() {
