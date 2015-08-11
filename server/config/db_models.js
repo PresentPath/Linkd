b8409a455384dbba6295fe2abbd6be872e4469a2
Katrina Uychaco <kuychaco@gmail.com>
2015-08-10 21:36:03 -0700

    Add header comments and export UserLink model

--- server/config/db_models.js
+++ server/config/db_models.js
@@ -1,9 +1,7 @@
-/* 
-* @Author: Katrina Uychaco
-* @Date:   2015-08-01 18:34:04
-* @Last Modified by:   kuychaco
-* @Last Modified time: 2015-08-08 22:02:51
-*/
+// Database Models
+// ----------------
+//
+// Connect to the database, define Models (create schema), and set up table associations.
 
 'use strict';
 
@@ -84,4 +82,5 @@ module.exports.Group = Group;
 module.exports.Folder = Folder;
 module.exports.Link = Link;
 module.exports.Comment = Comment;
+module.exports.UserLink = UserLink;
 module.exports.db = db;
