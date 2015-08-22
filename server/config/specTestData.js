78a64218b94be9947fa6931a2d2f4154d402aa17
Katrina Uychaco <kuychaco@gmail.com>
2015-08-21 21:33:10 -0700

    Add test folders

--- server/config/specTestData.js
+++ server/config/specTestData.js
@@ -1,32 +1,44 @@
-'use strict';
+// Spec Test Data
+// -----------
+//
+// Fake data for testing all routes and database controllers.
 
 var testGroups = [
-      {
-        name: 'testGroupA',
-        OwnerUserIdGoogle: '1'
-      },
-      {
-        name: 'testGroupB',
-        OwnerUserIdGoogle: '1'
-      }];
+  {
+    name: 'testGroupA',
+    OwnerUserIdGoogle: '1'
+  },
+  {
+    name: 'testGroupB',
+    OwnerUserIdGoogle: '1'
+  }];
 
 var testUsers = [{
-      user_id_google: '1',
-      name_google: 'testUser1',
-      email_google: 'user1@email.com'
-    },
-    {
-      user_id_google: '2',
-      name_google: 'testUser2',
-      email_google: 'user2@email.com'
-    },
-    {
-      user_id_google: '3',
-      name_google: 'testUser3',
-      email_google: 'user3@email.com'
-    }];
+    user_id_google: '1',
+    name_google: 'testUser1',
+    email_google: 'user1@email.com'
+  },
+  {
+    user_id_google: '2',
+    name_google: 'testUser2',
+    email_google: 'user2@email.com'
+  },
+  {
+    user_id_google: '3',
+    name_google: 'testUser3',
+    email_google: 'user3@email.com'
+  }];
 
-var testFolders = [];
+var testFolders = [{
+    name: 'testFolderX'
+  },
+  {
+    name: 'testFolderY'
+  },
+  {
+    name: 'testFolderZ'
+  }];
+
 var testLinks = [];
 var testComments = [];
 
