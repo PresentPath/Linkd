a3260233c640e51bcab9580802df2d60c93688af
Katrina Uychaco <kuychaco@gmail.com>
2015-08-21 18:22:02 -0700

    Create test data for mocha tests

---
+++ server/config/specTestData.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var testGroups = [
+      {
+        name: 'testGroupA',
+        OwnerUserIdGoogle: '1'
+      },
+      {
+        name: 'testGroupB',
+        OwnerUserIdGoogle: '1'
+      }];
+
+var testUsers = [{
+      user_id_google: '1',
+      name_google: 'testUser1',
+      email_google: 'user1@email.com'
+    },
+    {
+      user_id_google: '2',
+      name_google: 'testUser2',
+      email_google: 'user2@email.com'
+    },
+    {
+      user_id_google: '3',
+      name_google: 'testUser3',
+      email_google: 'user3@email.com'
+    }];
+
+
+module.exports.testGroups = testGroups;
+module.exports.testUsers = testUsers;
