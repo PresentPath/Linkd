2e644f2094077d3f128c9030b0354ef133945bea
Katrina Uychaco <kuychaco@gmail.com>
2015-08-21 23:47:22 -0700

    Add tests for create folder and get list

--- server/Folder/folderSpec.js
+++ server/Folder/folderSpec.js
@@ -16,8 +16,8 @@ var testFolders = require('../config/specTestData.js').testFolders;
 var testGroups = require('../config/specTestData.js').testGroups;
 var testUsers = require('../config/specTestData.js').testUsers;
 
+
 var deleteInstances = function(Model) {
-  console.log('!!!!!!!!delete')
   return Model.findAll()
     .then(function(instances) {
       return instances.map(function(instance) {
@@ -36,8 +36,8 @@ module.exports = function(callback) {
     var folderId;
 
     // Before tests, create a user and group for folder to belong to
-    before(function() {
-      return Promise.all([deleteInstances(User), deleteInstances(Group)])
+    beforeEach(function() {
+      return Promise.all([deleteInstances(User), deleteInstances(Group), deleteInstances(Folder)])
         .then(function(deleted) {
           return User.create(testUsers[0]);
         })
@@ -47,53 +47,96 @@ module.exports = function(callback) {
         .then(function(group) {
           return groupId = group.id;
         })
-        // .then(function() {
-        //   return Folder.create(testFolders[0]);
-        // });
+        .then(function() {
+          testFolders = testFolders.map(function(folder) {
+            folder.GroupId = groupId;
+            return folder;
+          });
+          return Folder.bulkCreate(testFolders);
+        });
     });
 
     // Before each test create new folders in database
-    beforeEach(function() {
+    // beforeEach(function() {
 
-      testFolders = testFolders.map(function(folder) {
-        return folder.GroupId = groupId;
-      });
+    //   testFolders = testFolders.map(function(folder) {
+    //     return folder.GroupId = groupId;
+    //   });
 
-      return deleteInstances(Folder)
-        .then(function(destroyed) {
-          console.log('FOLDER', typeof Folder.create);
-          return Folder.create(testFolders[0]);
+    //   return deleteInstances(Folder)
+    //     .then(function() {
+    //       console.log('FOLDER', typeof Folder.create);
+    //       return Folder.create(testFolders[0]);
+    //     })
+    //     .then(function(folder) {
+    //       console.log('FOLSERS', folder);
+    //     });
+    // });
+
+    it('should create a folder', function(done) {
+      // console.log('GROUPID', groupId);
+      request(app)
+        .post('/api/folder/create')
+        .send({
+          name: 'testFolderW'
         })
-        .then(function(folder) {
-          console.log('FOLSERS', folder);
+        .end(function(err, res) {
+          if (err) {
+            callback(err);
+            return done(err);
+          }
+          expect(res.body.name).to.equal('testFolderW');
+          Folder.find({ where: { name: 'testFolderW' } })
+            .then(function(folder) {
+              expect(folder).to.exist;
+              done();
+            });
         });
+
     });
 
     it('should create a folder', function(done) {
       // console.log('GROUPID', groupId);
-      // request(app)
-      //   .post('/api/folder/create')
-      //   .send({
-      //     name: 'testFolderW'
-      //   })
-      //   .end(function(err, res) {
-      //     if (err) {
-      //       callback(err);
-      //       return done(err);
-      //     }
-      //     expect(res.body.name).to.equal('testFolderW');
-      //     Folder.find({ where: { name: 'testFolderW' } })
-      //       .then(function(folder) {
-      //         expect(folder).to.exist;
-      //         done();
-      //       });
-      //   });
+      request(app)
+        .post('/api/folder/create')
+        .send({
+          name: 'testFolderW'
+        })
+        .end(function(err, res) {
+          if (err) {
+            callback(err);
+            return done(err);
+          }
+          expect(res.body.name).to.equal('testFolderW');
+          Folder.find({ where: { name: 'testFolderW' } })
+            .then(function(folder) {
+              expect(folder).to.exist;
+              done();
+            });
+        });
 
-    })
+    });
 
+    it('should get list of group\'s folders', function(done) {
+      // console.log('GROUPID', groupId);
+      request(app)
+        .get('/api/folder/group/'+groupId)
+        .end(function(err, res) {
+          if (err) {
+            callback(err);
+            return done(err);
+          }
+          var responseFolders = res.body.map(function(folder) {
+            return folder.name;
+          }).sort();
+          var expectedFolders = testFolders.map(function(folder) {
+            return folder.name;
+          }).sort();
+          expect(responseFolders).to.eql(expectedFolders);
+          done();
+        });
 
-
-
+    });
 
 
     // callback();
