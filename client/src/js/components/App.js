44657ed4e5a817edbc760ed86150cc45019cf2fe
Katrina Uychaco <kuychaco@gmail.com>
2015-08-31 01:14:48 -0700

    Load initial data from server

--- client/src/js/components/App.js
+++ client/src/js/components/App.js
@@ -30,9 +30,7 @@ let App = React.createClass({
   
   getGroups () {
     return Promise.resolve(
-      $.ajax({
-        url: '/api/group/user/1'
-      })
+      $.ajax({ url: '/api/group/user/1' })
     )
     .tap((groups) => {
       this.setState({ groups });
@@ -42,33 +40,77 @@ let App = React.createClass({
     });
   },
 
-  getFolders (groups) {
-    var groupPromises = groups.map((group) => {
-      return Promise.resolve($.ajax({
-          url: '/api/folder/group/' + group.id
-        }))
+  getFolders () {
+    let groups = this.state.groups;
+    let groupPromises = groups.map((group) => {
+      return Promise.resolve($.ajax({ url: '/api/folder/group/' + group.id }))
         .then((folders) => {
-          console.log('folders list for group', group.id);
           this.state.folders[group.id] = folders;
           this.setState({ folders: this.state.folders })
-          console.log('state folders', this.state.folders);
         });
-    })
+    });
     return Promise.all(groupPromises)
     .catch((err) => {
       console.error('Error getting groups list', status, err.toString());
     });
   },
 
+  getLinks () {
+    return Promise.resolve(
+      $.ajax({ url: '/api/link/user/1' })
+    )
+    .tap((links) => {
+      links.forEach((link) => {
+        this.state.links[link.FolderId] = this.state.links[link.FolderId] || [];
+        this.state.links[link.FolderId].push(link); 
+      });
+      this.setState({ links: this.state.links });
+    })
+    .catch((err) => {
+      console.error('Error getting links list', status, err.toString());
+    });
+  },
+
+  getComments () {
+    let groups = this.state.groups;
+    let groupPromises = groups.map((group) => {
+      return Promise.resolve($.ajax({ url: '/api/comment/group/' + group.id }))
+        .then((comments) => {
+          this.state.comments[group.id] = {};
+          let commentList = this.state.comments[group.id];
+          comments.forEach((comment) => {
+            commentList[comment.LinkId] = commentList[comment.LinkId] || [];
+            // Can we assume comments will be in order? Sorted in terms of primary key and thus time added...
+            commentList[comment.LinkId].push(comment);
+          });
+          this.setState({ comments: this.state.comments })
+        });
+    });
+    return Promise.all(groupPromises)
+    .catch((err) => {
+      console.error('Error getting comments list', status, err.toString());
+    });
+  },
+
   componentDidMount () {
-    console.log('make request for groups');
     this.getGroups()
-      .then((groups) => {
-        console.log('saved groups list', groups);
-        return this.getFolders(groups);
+      .then(() => {
+        console.log('saved groups list');
+        return this.getFolders();
       })
       .then(() => {
         console.log('saved folders list for all groups');
+        return this.getLinks();
+      })
+      .then(() => {
+        console.log('saved links list for all folders');
+        return this.getComments();
+      })
+      .then(() => {
+        console.log('saved comments list for all links');
+      })
+      .catch((err) => {
+        console.error('Error fetching data', status, err.toString());
       });
   },
 
