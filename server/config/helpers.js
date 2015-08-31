587653c466881de9183eeb68c23124a81310dbab
Katrina Uychaco <kuychaco@gmail.com>
2015-08-30 17:21:42 -0700

    Add then and catch to deleteInstances

--- server/config/helpers.js
+++ server/config/helpers.js
@@ -35,6 +35,13 @@ module.exports.deleteInstances = function(Model) {
     .then(function(instances) {
       return instances.map(function(instance) {
         return instance.destroy();
-      });
+      })
+    })
+    .then(function(deleted) {
+      console.log('Dropped table for', Model);
+      return deleted;
+    })
+    .catch(function(err) {
+      console.error('Error deleting all instances', err.message);
     });
 };
