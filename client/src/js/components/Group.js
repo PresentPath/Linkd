40b1a23cdbb3f3dc537544a6699ad2d725db1833
Katrina Uychaco <kuychaco@gmail.com>
2015-09-01 15:03:58 -0700

    Render Group component - in progress

--- client/src/js/components/Group.js
+++ client/src/js/components/Group.js
@@ -3,15 +3,43 @@
 import React from 'react';
 import FolderList from './FolderList';
 
+/*
+Group
+- props: 
+  - updateGroup
+  - updateFolder
+  - updateLink
+  - folders
+  - links
+ */
+
 let Group = React.createClass({
-  
+
   render () {
+    let group = this.props.group;  
+
+    let userList = group.Users.reduce((list, user) => {
+      return list + user.name_google + ', ';
+    }, '').slice(0, -2);
+
+    let contents = group.isLoaded ? (
+      <div className="groupContents" visibiliy={group.visibility}> 
+        <span className="userList"> {userList} </span> 
+        <FolderList 
+          folders={this.props.folders}
+          links={this.props.links}
+          updateFolder={this.props.updateFolder}
+          updateLink={this.props.updateLink} />
+      </div>
+    ) : undefined; 
+
     return (
-      <div>
-        Group
-        <FolderList/>
+      <div className="group">
+        <h4 onClick={this.props.updateGroup.bind(null, group)}> {group.name} </h4>
+        {contents}
       </div>
     );
+
   }
 });
 
