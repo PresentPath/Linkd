19e5fa845d4c6be8d4d677bee66f9efed65c6c0d
Katrina Uychaco <kuychaco@gmail.com>
2015-09-01 15:04:24 -0700

    Pass group as prop to Group component

--- client/src/js/components/GroupList.js
+++ client/src/js/components/GroupList.js
@@ -6,17 +6,20 @@ import Group from './Group';
 let GroupList = React.createClass({
   
   render () {
+
     let groupNodes = this.props.groups.map((group) => {
       return (
         <Group 
           key={group.id}
+          group={group}
           folders={this.props.folders}
           links={this.props.links} 
           updateGroup={this.props.updateGroup}
           updateFolder={this.props.updateFolder}
           updateLink={this.props.updateLink} />
       );
-    })
+    });
+
     return (
       <div className="groupList">
         <h3>GroupList</h3>
