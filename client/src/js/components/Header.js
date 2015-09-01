d7df498aa9b16296d3805cf701487f4af0631ec8
Katrina Uychaco <kuychaco@gmail.com>
2015-09-01 11:17:23 -0700

    Display user name

--- client/src/js/components/Header.js
+++ client/src/js/components/Header.js
@@ -8,6 +8,7 @@ let Header = React.createClass({
     return (
       <div className="page-header text-center">
           <h1> Welcome to Linkd! </h1>
+          Hello {this.props.user.name_google}! Share bookmarks with your friends! <br/>
           <a href="/api/user/logout" className="btn btn-default btn-sm">Logout</a>
       </div>
     );
