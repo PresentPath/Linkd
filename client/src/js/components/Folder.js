cd783ffd05a103f0dd09bf4d3a190c079cb4af9b
Katrina Uychaco <kuychaco@gmail.com>
2015-09-02 16:46:47 -0700

    Partially implement rendering

--- client/src/js/components/Folder.js
+++ client/src/js/components/Folder.js
@@ -5,12 +5,31 @@ import FolderList from './FolderList';
 import LinkList from './LinkList';
 
 let Folder = React.createClass({
-  
+
   render () {
-    return (
-      <div>
-        Folder
+    
+    let folder = this.props.folder;
+
+    let style = {
+      display: folder.display
+    };
+
+    let contents = folder.isRendered ? (
+      <div className="folderContents" style={style}> 
+        <FolderList 
+          parentFolderId={this.props.key}
+          folders={this.props.folders}
+          links={this.props.links}
+          updateFolder={this.props.updateFolder}
+          updateLink={this.props.updateLink} />
         <LinkList/>
+      </div>
+    ) : undefined;
+
+    return (
+      <div className="folder">
+        <h4 onClick={this.props.updateFolder.bind(null, folder)}> {folder.name} </h4>
+        {contents}
       </div>
     );
   }
--- client/src/js/components/FolderList.js
+++ client/src/js/components/FolderList.js
@@ -3,13 +3,40 @@
 import React from 'react';
 import Folder from './Folder';
 
+/*
+FolderList
+- props:
+  - updateFolder
+  - updateLink
+  - folders
+  - links
+
+ */
+
+
 let FolderList = React.createClass({
   
   render () {
+
+    let childFolders = this.props.folders.filter((folder) => {
+      return folder.ParentId === this.props.parentFolderId;
+    });
+
+    let folderNodes = childFolders.map((folder) => {
+      return (
+        <Folder
+          key={folder.id}
+          folder={folder}
+          folders={this.props.folders}
+          links={this.props.links}
+          updateFolder={this.props.updateFolder}
+          updateLink={this.props.updateLink} />
+      );
+    });
+
     return (
       <div>
-        FolderList
-        <Folder/>
+        {folderNodes}
       </div>
     );
   }
