5997fcabd286dff1ec177dfa6d6133ef8d871767
Katrina Uychaco <kuychaco@gmail.com>
2015-09-04 19:11:33 -0700

    Render link details

--- client/src/js/components/LinkDetail.js
+++ client/src/js/components/LinkDetail.js
@@ -7,11 +7,18 @@ import CommentForm from './CommentForm';
 let LinkDetail = React.createClass({
   
   render () {
+
+    let link = this.props.currentLink;
+
+    console.log('comments in link detail',this.props.comments);
+
     return (
-      <div>
-        LinkDetail
-        <CommentList/>
-        <CommentForm addComment={this.props.addComment}/>
+      <div className="linkDetail">
+        <div className="linkName"> {link.name} </div>
+        <div className="linkURL"> <a href={link.url}> {link.url} </a> </div>
+        <CommentList comments={this.props.comments} />
+        <hr />
+        <CommentForm addComment={this.props.addComment} />
       </div>
     );
   }
