d8408fab8d65c4c1941d0befe65eda427fb2f0ff
Katrina Uychaco <kuychaco@gmail.com>
2015-09-04 19:11:13 -0700

    Render comment list

--- client/src/js/components/CommentList.js
+++ client/src/js/components/CommentList.js
@@ -5,9 +5,22 @@ import React from 'react';
 let CommentList = React.createClass({
   
   render () {
+
+    let commentNodes = this.props.comments.map((comment) => {
+      return (
+        <div key={comment.id} className="comment">
+          <div>
+            <span className="commentAuthor"> {comment.User.name_google} </span> 
+            <span className="commentTimestamp"> {comment.updatedAt} </span>
+          </div>
+          <div className="commentText"> {comment.text} </div>
+        </div>
+      );
+    });
+
     return (
-      <div>
-        CommentList
+      <div className="commentList">
+        {commentNodes}
       </div>
     );
   }
