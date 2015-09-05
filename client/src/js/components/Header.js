66897446210b4d5b93ecbad03a3565d2715768f7
Katrina Uychaco <kuychaco@gmail.com>
2015-09-05 14:22:20 -0700

    Add basic styling for header

--- client/src/js/components/Header.js
+++ client/src/js/components/Header.js
@@ -8,9 +8,14 @@ let Header = React.createClass({
   render () {
     return (
       <div className="header page-header text-center">
-          <h1> Welcome to Linkd! </h1>
-          Hello {this.props.user.name_google}! Share bookmarks with your friends! <br/>
-          <a href="/api/user/logout" className="btn btn-default btn-sm">Logout</a>
+          <h1> Linkd </h1>
+          
+          <div className="logout">
+            <span className="greeting">Welcome, {this.props.user.name_google}</span>
+            <a href="/api/user/logout" className="btn btn-default btn-sm">Logout</a>
+          </div>
+
+          <h4 className="tagline">Share bookmarks with your friends! </h4>
       </div>
     );
   }
