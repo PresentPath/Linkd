9efadfd3f60afabc64532dffb028b7677da492ff
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-09-08 14:03:10 -0700

    Can add links without http prefix and still works

--- client/src/js/components/LinkDetail.js
+++ client/src/js/components/LinkDetail.js
@@ -12,10 +12,12 @@ let LinkDetail = React.createClass({
 
     let link = this.props.currentLink;
 
+    let url = link.url.search(/^http:\/\//) !== 0 ? 'http://' + link.url : link.url;
+
     return (
       <div className="linkDetail">
         <div className="linkName"> {link.name} </div>
-        <div className="linkURL"> <a href={link.url} target="_blank"> {link.url} </a> </div>
+        <div className="linkURL"> <a href={url} target="_blank"> {url} </a> </div>
         <CommentList comments={this.props.comments} />
         <CommentForm addComment={this.props.addComment} />
       </div>
