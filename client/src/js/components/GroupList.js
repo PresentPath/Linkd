2ce1a92d77225e54c908050a4bd1c90e376144f2
Katrina Uychaco <kuychaco@gmail.com>
2015-09-13 16:18:55 -0700

    Refactor to use flux

--- client/src/js/components/GroupList.js
+++ client/src/js/components/GroupList.js
@@ -2,32 +2,61 @@
 
 import React from 'react';
 import Group from './Group';
+
+import GroupStore from '../stores/GroupStore';
+
 import '../../stylesheets/components/groupList.scss';
 
+function getStateFromStores() {
+  return {
+    groups: GroupStore.getGroups()
+  };
+};
+
+function getGroupListItem(group) {
+  return (
+    <p> {group.id} </p>
+  );
+  // return (
+  //   <Group 
+  //     key={group.id}
+  //     group={group} />
+  // );
+};
 
 let GroupList = React.createClass({
-  
+
+  getInitialState () {
+    return getStateFromStores();
+  },
+
+  componentDidMount () {
+    GroupStore.addChangeListener(this._onChange);
+  },
+
+  componentWillUnmount () {
+    GroupStore.removeChangeListener(this._onChange);
+  },
+
   render () {
 
-    let groupNodes = this.props.groups.map((group) => {
-      return (
-        <Group 
-          key={group.id}
-          group={group}
-          folders={this.props.folders}
-          links={this.props.links} 
-          updateGroup={this.props.updateGroup}
-          updateFolder={this.props.updateFolder}
-          updateLink={this.props.updateLink} />
-      );
-    });
+    let groups = this.state.groups;
+
+    let groupListItems = Object.keys(groups).map((groupId) => {
+        return getGroupListItem(groups[groupId]);
+      });
 
     return (
       <div className="groupList">
-        {groupNodes}
+        {groupListItems}
       </div>
     );
+  },
+
+  _onChange () {
+    this.setState(getStateFromStores());
   }
+
 });
 
 export default GroupList;
