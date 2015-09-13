e585bdf70ee89350d83e5fd9689b3705f90c32ad
Katrina Uychaco <kuychaco@gmail.com>
2015-09-13 16:17:32 -0700

    Create header component

--- client/src/js/components/Header.js
+++ client/src/js/components/Header.js
@@ -1,9 +1,30 @@
 'use strict';
 
 import React from 'react';
+
+import UserStore from '../stores/UserStore';
+
 import '../../stylesheets/components/header.scss';
 
+function getStateFromStores() {
+  return {
+    user: UserStore.getUser()
+  }
+};
+
 let Header = React.createClass({
+
+  getInitialState () {
+    return getStateFromStores();
+  },
+
+  componentDidMount () {
+    UserStore.addChangeListener(this._onChange);
+  },
+
+  componentWillUnmount () {
+    UserStore.removeChangeListener(this._onChange);
+  },
   
   render () {
     return (
@@ -11,14 +32,19 @@ let Header = React.createClass({
           <h1> &lt;&gt; Linkd </h1>
           
           <div className="logout">
-            <span className="greeting">Welcome, {this.props.user.name_google}</span>
+            <span className="greeting">Welcome, {this.state.user.name_google}</span>
             <a href="/api/user/logout" className="logoutButton btn btn-default btn-sm">Logout</a>
           </div>
 
           <h4 className="tagline">Share bookmarks with your friends! </h4>
       </div>
     );
+  },
+
+  _onChange () {
+    this.setState(getStateFromStores());
   }
+
 });
 
 export default Header;
