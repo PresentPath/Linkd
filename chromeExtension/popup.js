78163f32406632cdf7368829ff580fb1fdd01906
Katrina Uychaco <kuychaco@gmail.com>
2015-09-23 19:52:49 -0700

    Style plugin

--- chromeExtension/popup.js
+++ chromeExtension/popup.js
@@ -1,13 +1,19 @@
 $(function() {
   $('#addLink').submit(function(event) {
     event.preventDefault();
-    console.log('sdfdsf');
+
+    var name = $('#name').val();
+    var folder = $('#folder').val();
+
+    $('input').hide();
+    $('body').append('<p>' + name + ' added to ' + folder + '</p>');
     
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 
+
       var data = {
-        folder: $('#folder').val(),
-        name: $('#name').val(),
+        folder: folder,
+        name: name,
         url: tabs[0].url
       };
 
